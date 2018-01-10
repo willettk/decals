f7065e0f7fcf86618458f099e381dff3222844e6
Mike Walmsley <walmsleymk1@gmail.com>
2018-01-10 09:52:20 +0000

    Temporarily deactivate datashader - I don't know how to use conda with Travis

--- python/shared_utilities.py
+++ python/shared_utilities.py
@@ -4,56 +4,57 @@ from astropy.coordinates import SkyCoord
 from astropy import units as u
 from astropy import table
 
-import datashader as ds
-import datashader.transfer_functions as tf
-from datashader.utils import export_image
+# don't know how to install conda modules on travis
+# import datashader as ds
+# import datashader.transfer_functions as tf
+# from datashader.utils import export_image
 
 
-def plot_catalog(catalog, filename, column_to_colour=None):
-    """
-
-    Args:
-        catalog ():
-        filename ():
-        column_to_colour ():
-
-    Returns:
-
-    """
-    canvas = ds.Canvas(plot_width=300, plot_height=300)
-    if column_to_colour:  # shade pixels by average value in column_to_colour
-        aggc = canvas.points(catalog, 'ra', 'dec', ds.mean(column_to_colour))
-    else:
-        aggc = canvas.points(catalog, 'ra', 'dec')
-    img = tf.shade(aggc)
-    export_image(img, filename)
-
-
-def plot_catalog_overlap(catalog_a, catalog_b, legend, filename):
-    """
-
-    Args:
-        catalog_a ():
-        catalog_b ():
-        legend ():
-        filename ():
-
-    Returns:
-
-    """
-
-    a_coords = catalog_a[['ra', 'dec']]
-    a_coords['catalog'] = legend[0]
-    b_coords = catalog_b[['ra', 'dec']]
-    b_coords['catalog'] = legend[1]
-
-    df_to_plot = pd.concat([a_coords, b_coords])
-    df_to_plot['catalog'] = df_to_plot['catalog'].astype('category')
-
-    canvas = ds.Canvas(plot_width=300, plot_height=300)
-    aggc = canvas.points(df_to_plot, 'ra', 'dec', ds.count_cat('catalog'))
-    img = tf.shade(aggc)
-    export_image(img, filename)
+# def plot_catalog(catalog, filename, column_to_colour=None):
+#     """
+#
+#     Args:
+#         catalog ():
+#         filename ():
+#         column_to_colour ():
+#
+#     Returns:
+#
+#     """
+#     canvas = ds.Canvas(plot_width=300, plot_height=300)
+#     if column_to_colour:  # shade pixels by average value in column_to_colour
+#         aggc = canvas.points(catalog, 'ra', 'dec', ds.mean(column_to_colour))
+#     else:
+#         aggc = canvas.points(catalog, 'ra', 'dec')
+#     img = tf.shade(aggc)
+#     export_image(img, filename)
+#
+#
+# def plot_catalog_overlap(catalog_a, catalog_b, legend, filename):
+#     """
+#
+#     Args:
+#         catalog_a ():
+#         catalog_b ():
+#         legend ():
+#         filename ():
+#
+#     Returns:
+#
+#     """
+#
+#     a_coords = catalog_a[['ra', 'dec']]
+#     a_coords['catalog'] = legend[0]
+#     b_coords = catalog_b[['ra', 'dec']]
+#     b_coords['catalog'] = legend[1]
+#
+#     df_to_plot = pd.concat([a_coords, b_coords])
+#     df_to_plot['catalog'] = df_to_plot['catalog'].astype('category')
+#
+#     canvas = ds.Canvas(plot_width=300, plot_height=300)
+#     aggc = canvas.points(df_to_plot, 'ra', 'dec', ds.count_cat('catalog'))
+#     img = tf.shade(aggc)
+#     export_image(img, filename)
 
 # pandas version
 # def match_galaxies_to_catalog(galaxies, catalog, matching_radius=10 * u.arcsec):
