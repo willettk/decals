7d80f920a0f13f2fdf5da1cf54fb36f71b13fb61
Mike Walmsley <walmsleymk1@gmail.com>
2018-01-16 14:31:12 +0000

    Update unit tests to ignore RA/DEC - new expected behaviour

--- python/a_download_decals/setup/join_brick_tables_test.py
+++ python/a_download_decals/setup/join_brick_tables_test.py
@@ -17,16 +17,6 @@ def coordinate_catalog():
          'dec': 12.0,
          'example_edge': 1.5,
          'brickname': 'bad_name'},
-
-        {'ra': 200.0,
-         'dec': 120.0,
-         'example_edge': 199.5,
-         'brickname': 'bad_ra'},
-
-        {'ra': 15.0,
-         'dec': 15.0,
-         'example_edge': 14.5,
-         'brickname': 'bad_dec_tiny_value'},
     ])
 
 
@@ -43,16 +33,6 @@ def exposure_catalog():
          'dec': 12.0,
          'exposures': 0,
          'brickname': 'bad_name_different'},  # name is not the same - should not join
-
-        {'ra': 150.0,  # ra is not the same - should not join
-         'dec': 120.0,
-         'exposures': 2,
-         'brickname': 'bad_ra'},
-
-        {'ra': 15.0,
-         'dec': 15.0000001,  # dec is not the exactly same - should not join
-         'exposures': 1,
-         'brickname': 'bad_dec_tiny_value'}
     ])
 
 
@@ -62,5 +42,3 @@ def test_merge_brick_tables(coordinate_catalog, exposure_catalog):
 
     assert 'simple_match' in matched_bricks
     assert 'bad_name' not in matched_bricks
-    assert 'bad_ra' not in matched_bricks
-    assert 'bad_dec_tiny_value' not in matched_bricks
