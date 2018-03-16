c401fc2d9e0519c5b82c2838924aceec0e008bd2
Mike Walmsley <walmsleymk1@gmail.com>
2018-03-16 16:39:50 +0000

    finish dr1/2 setup, minor tweaks to uploader

--- python/b_to_zooniverse/previous_subjects/previous_decals_subjects_test.py
+++ python/b_to_zooniverse/previous_subjects/previous_decals_subjects_test.py
@@ -1,15 +1,16 @@
 import pytest
 
+import json
+
 import numpy as np
 import pandas as pd
 from astropy.table import Table
 
-
-from b_to_zooniverse.previous_subjects.previous_decals_subjects import link_previous_subjects_with_nsa
+from b_to_zooniverse.previous_subjects.previous_decals_subjects import link_previous_subjects_with_nsa, get_previous_decals_subjects
 
 
 @pytest.fixture()
-def galaxy_zoo():
+def decals_subjects():
     return pd.DataFrame([
         # DR1 entries have provided_image_id filled with iau name, nsa_id blank, dr blank
         {'_id': 'ObjectId(0)',
@@ -31,23 +32,55 @@ def galaxy_zoo():
 
 
 @pytest.fixture()
+def galaxy_zoo_subjects():
+    return pd.DataFrame([
+        # DR1 entries have provided_image_id filled with iau name, nsa_id blank, dr blank
+        {'_id': 'ObjectId(0)',
+         'zooniverse_id': 'gz_a',
+         'group_id': 'group_id_a',
+         'metadata': json.dumps({
+             'survey': 'decals',
+             'provided_image_id': 'iau_a',
+             'counters': {'feature': 0,
+                          'smooth': 0,
+                          'star': 0}})
+         },
+
+
+        # DR2 entries have provided_image_id blank, nsa_id filled with NSA_[number], dr filled with 'DR2'
+        {'_id': 'ObjectId(1)',
+         'zooniverse_id': 'gz_b',
+         'group_id': 'group_id_b',
+         'metadata': json.dumps({
+             'nsa_id': 'NSA_1',
+             'dr': 'DR2',
+             'counters': {'feature': 0,
+                          'smooth': 0,
+                          'star': 0}})
+         },
+    ])
+
+
+@pytest.fixture()
 def nsa():
     return Table([
 
         {'iauname': 'iau_a',
          'nsa_id': '0',
          'ra': 10.,
-         'dec': -1.},
+         'dec': -1.,
+         'nsa_version': '1_0_0'},
 
         {'iauname': 'iau_b',
          'nsa_id': '1',
          'ra': 10.,
-         'dec': -1.}
+         'dec': -1.,
+         'nsa_version': '1_0_0'}
     ])
 
 
-def test_get_galaxy_zoo_decals_catalog_with_nsa(galaxy_zoo, nsa):
-    galaxy_zoo_with_nsa = link_previous_subjects_with_nsa(galaxy_zoo, nsa)
+def test_get_galaxy_zoo_decals_catalog_with_nsa(decals_subjects, nsa):
+    galaxy_zoo_with_nsa = link_previous_subjects_with_nsa(decals_subjects, nsa)
 
     target_df = pd.DataFrame([
         {'zooniverse_id': 'gz_a',
@@ -74,3 +107,9 @@ def test_get_galaxy_zoo_decals_catalog_with_nsa(galaxy_zoo, nsa):
     for zooniverse_id in galaxy_zoo_with_nsa.index:
         for column in galaxy_zoo_with_nsa.columns.values:
             assert galaxy_zoo_with_nsa.loc[zooniverse_id][column] == target_df.loc[zooniverse_id][column]
+
+
+def test_get_previous_decals_subjects(galaxy_zoo_subjects, nsa):
+    galaxy_zoo_subjects.to_csv('fake_subjects.csv')
+    cleaned_decals_subjects = get_previous_decals_subjects(galaxy_zoo_subjects, nsa)
+    print(cleaned_decals_subjects)
--- python/b_to_zooniverse/upload_decals.py
+++ python/b_to_zooniverse/upload_decals.py
@@ -35,6 +35,7 @@ def upload_decals_to_panoptes(joint_catalog,
         None
     """
     joint_catalog = joint_catalog[joint_catalog['png_ready'] == True]
+    joint_catalog = joint_catalog[joint_catalog['fits_filled'] == True]
 
     dr2_galaxies, dr5_only_galaxies = match_galaxies_to_catalog_table(  # unmatched galaxies are new
         galaxies=joint_catalog,
@@ -47,31 +48,38 @@ def upload_decals_to_panoptes(joint_catalog,
     # use Nair galaxies previously classified in DR2
     calibration_catalog = get_expert_catalog_joined_with_decals(dr2_galaxies, expert_catalog)
 
-    # upload standard calibration set of Nair/DR2 galaxies, coloured by DR1/2 rules
     # calibration_set_name = 'decals_dr2_nair_calibration_dr2_style_250_each'
     # calibration_catalog_dr2_style = make_catalog_png_images(
     #     calibration_catalog,
     #     image_utils.get_dr2_style_image,
     #     '{}/{}'.format(calibration_dir, calibration_set_name),
     #     size=424,
-    #     overwrite=overwrite_png)
+    #     overwrite=False)
+
+    # upload standard calibration set of Nair/DR2 galaxies, coloured by DR1/2 rules
     # _ = upload_subject_set.upload_nair_calibration_subject_set(
     #     calibration_catalog_dr2_style, calibration_set_name)
 
     # upload all Nair/DR2 galaxies, coloured by Lupton rules
-    calibration_set_name = 'decals_dr2_nair_lupton_style_all'
-    calibration_catalog_lupton_style = make_catalog_png_images(
-        calibration_catalog,
-        image_utils.get_colour_style_image,
-        '{}/{}'.format(calibration_dir, calibration_set_name),
-        size=424,
-        overwrite=True)  # places new png in calibration folder under this name
-    upload_subject_set.upload_galaxy_subject_set(
-        calibration_catalog_lupton_style, calibration_set_name)
+    # calibration_set_name = 'decals_dr2_nair_lupton_style_all'
+    # calibration_catalog_lupton_style = make_catalog_png_images(
+    #     calibration_catalog,
+    #     image_utils.get_colour_style_image,
+    #     '{}/{}'.format(calibration_dir, calibration_set_name),
+    #     size=424,
+    #     overwrite=True)  # places new png in calibration folder under this name
+    # upload_subject_set.upload_galaxy_subject_set(
+    #     calibration_catalog_lupton_style, calibration_set_name)
 
     # upload all Nair/DR2 galaxies, coloured by DR2 rules
-    # calibration_set_name = 'decals_dr2_nair_dr2_style_all'
-    # _ = upload_subject_set.upload_galaxy_subject_set(calibration_catalog, calibration_set_name)
+    calibration_set_name = 'decals_dr2_nair_dr2_style_all'
+    calibration_catalog_dr2_style = make_catalog_png_images(
+        calibration_catalog,
+        image_utils.get_dr2_style_image,
+        '{}/{}'.format(calibration_dir, calibration_set_name),
+        size=424,
+        overwrite=False)
+    _ = upload_subject_set.upload_galaxy_subject_set(calibration_catalog_dr2_style, calibration_set_name)
 
     # upload first n DR2-only galaxies
     # dr2_only_name = 'first_1k_decals_dr2'
@@ -80,13 +88,13 @@ def upload_decals_to_panoptes(joint_catalog,
     # upload first n DR5-only galaxies
     # dr5_only_name = 'first_1k_decals_dr5_only'
     # _ = upload_subject_set.upload_galaxy_subject_set(dr5_only_galaxies[:1000], dr5_only_name)
-    # dr5_only_name = 'first_10k_decals_dr5_only'
-    # _ = upload_subject_set.upload_galaxy_subject_set(dr5_only_galaxies[:10000], dr5_only_name)
+    dr5_only_name = '10k_to_30k_decals_dr5_only'
+    _ = upload_subject_set.upload_galaxy_subject_set(dr5_only_galaxies[10000:30000], dr5_only_name)
 
 
 if __name__ == '__main__':
 
-    settings.new_previous_subjects = False
+    settings.new_previous_subjects = True
 
     joint_catalog = Table(fits.getdata(settings.joint_catalog_loc))
     joint_catalog = enforce_joint_catalog_columns(joint_catalog, overwrite_cache=False)
@@ -94,7 +102,7 @@ if __name__ == '__main__':
     expert_catalog = get_expert_catalog(settings.expert_catalog_loc)
 
     if settings.new_previous_subjects:
-        raw_previous_subjects = pd.read_csv(settings.previous_subjects_loc)  # TODO make clear how to produce this
+        raw_previous_subjects = pd.read_csv(settings.previous_subjects_loc)  # MongoDB subject dump to csv from Ouro.
         nsa_v1_0_0 = get_nsa_catalog(settings.nsa_v1_0_0_catalog_loc, '1_0_0')  # takes a while
         previous_subjects = get_previous_decals_subjects(raw_previous_subjects, nsa_v1_0_0)
         previous_subjects.write(settings.subject_loc, overwrite=True)
@@ -102,6 +110,7 @@ if __name__ == '__main__':
         previous_subjects_df = pd.read_csv(settings.subject_loc)
         previous_subjects = Table.from_pandas(previous_subjects_df)  # previously extracted decals subjects
 
+    exit(0)
     upload_decals_to_panoptes(
         joint_catalog,
         previous_subjects,
